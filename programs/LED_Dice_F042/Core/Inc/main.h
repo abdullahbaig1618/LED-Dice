c4d0a5812ccab3a362d956ac37828286c9476d34
abdullahbaig1618 <abdullahbaig1618@gmail.com>
2026-06-21 18:01:55 -0600

    schematic is complete

--- programs/LED_Dice_F042/Core/Inc/main.h
+++ programs/LED_Dice_F042/Core/Inc/main.h
@@ -49,6 +49,8 @@ extern "C" {
 
 /* USER CODE END EM */
 
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
 /* Exported functions prototypes ---------------------------------------------*/
 void Error_Handler(void);
 
@@ -57,6 +59,10 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define touch_Pin GPIO_PIN_2
+#define touch_GPIO_Port GPIOA
+#define Vbatt_Pin GPIO_PIN_4
+#define Vbatt_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
